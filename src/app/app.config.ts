e0dfbb2d54152d64d2b073baa59473548b2d509e
Moumen Negm <mnegm2@student.tgm.ac.at>
2025-12-12 12:11:57 +0100

    troubleshooting app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideAuth, getAuth, connectAuthEmulator } from '@angular/fire/auth';
@@ -11,14 +11,12 @@ import { provideRouter } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    importProvidersFrom(
       provideFirebaseApp(() => initializeApp(environment.firebase)),
       provideFirestore(() => getFirestore()),
       provideAuth(() => getAuth()),
       provideFunctions(() => getFunctions()),
       provideStorage(() => getStorage()),
-      provideMessaging(() => getMessaging())
-    ),
-    provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"friendlychat-12896","appId":"1:591022000648:web:8d12460a931ca4fc479fbd","storageBucket":"friendlychat-12896.firebasestorage.app","apiKey":"AIzaSyCn5Q-0Z4GWD4CbPLPOWatFPSnpCr4FfXI","authDomain":"friendlychat-12896.firebaseapp.com","messagingSenderId":"591022000648","measurementId":"G-T854C0GZTJ","projectNumber":"591022000648","version":"2"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())
+      provideMessaging(() => getMessaging()),
+    provideRouter(routes)
   ],
 };
