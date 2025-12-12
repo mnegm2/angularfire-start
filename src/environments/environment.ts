bb305a0b155445010be83f219c9a57bf6e0587ab
Moumen Negm <mnegm2@student.tgm.ac.at>
2025-12-12 12:54:17 +0100

    Adding Google Authentication

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,11 @@
 export const environment = {
   firebase: {
-    apiKey: 'API_KEY',
-    authDomain: 'PROJECT_ID.firebaseapp.com',
-    databaseURL: 'https://PROJECT_ID.firebaseio.com',
-    projectId: 'PROJECT_ID',
-    storageBucket: 'PROJECT_ID.appspot.com',
-    messagingSenderId: 'SENDER_ID',
-    appId: 'APP_ID',
-    measurementId: 'G-MEASUREMENT_ID',
+    apiKey: "AIzaSyCn5Q-0Z4GWD4CbPLPOWatFPSnpCr4FfXI",
+    authDomain: "friendlychat-12896.firebaseapp.com",
+    projectId: "friendlychat-12896",
+    storageBucket: "friendlychat-12896.firebasestorage.app",
+    messagingSenderId: "591022000648",
+    appId: "1:591022000648:web:8d12460a931ca4fc479fbd",
+    measurementId: "G-T854C0GZTJ",
   },
 };
---
+++ src/firebase-config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCn5Q-0Z4GWD4CbPLPOWatFPSnpCr4FfXI",
+  authDomain: "friendlychat-12896.firebaseapp.com",
+  projectId: "friendlychat-12896",
+  storageBucket: "friendlychat-12896.firebasestorage.app",
+  messagingSenderId: "591022000648",
+  appId: "1:591022000648:web:8d12460a931ca4fc479fbd",
+  measurementId: "G-T854C0GZTJ"
+};
